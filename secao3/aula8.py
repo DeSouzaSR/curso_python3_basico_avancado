7d40595092815c1c90c3d955d52480491be42379
DeSouzaSR <sandro.fisica@gmail.com>
2026-06-12 15:50:15 -0300

    exercício da aula 8

---
+++ secao3/aula8.py
@@ -0,0 +1,19 @@
+nome = 'Sandro'
+sobrenome = 'De Souza'
+ano_nascimento = 1972
+ano_atual = 2026
+altura_metros = 1.70
+
+idade = ano_atual - ano_nascimento
+
+if idade >= 18:
+    maior_de_idade = 'Sim'
+else:
+    maior_de_idade = 'Não'
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade? ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
